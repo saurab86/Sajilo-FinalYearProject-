9b262114fc3a8a3aa444e94328eb9104502fb02e
Sisir <sisirkhanal20@gmail.com>
2021-05-20 14:16:21 +0545

    Hi

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-
-
 const Color primaryColor = Color(0xFFFB6158);
 const Color accentColor = Color(0xFFFFFAF7);
 const Color backgroundColor = Color(0xFFFFF6F3);
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
-//import 'package:sajilo/screens/welcome/welcome.dart';
 import 'package:sajilo/screens/welcome/welcome1.dart';
-//import 'navpages/navscreens/navhandling.dart';
 import 'services/auth.dart';
-// import 'package:sajilo/theme.dart';'
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MyApp());
 }
-
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -23,8 +17,7 @@ class MyApp extends StatelessWidget {
           create: (_) => AuthService(),
         ),
         StreamProvider(
-            create: (contex) => contex.read<AuthService>().authStateChanges),
-            
+            create: (contex) => contex.read<AuthService>().authStateChanges), 
       ],
       child: MaterialApp(
         title: 'Sajilo',
